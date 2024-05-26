d238b19953ba42d9cebe3ab276b841c55be206c5
chnpngwng <107300257+chnpngwan@users.noreply.github.com>
2024-05-26 21:25:40 +0800

    update

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Springboot_Example
 spring boot的一些集成案例
 # environment
-1. JDK17
-2. Spring Boot 3
+1. JDK1.8
+2. Spring Boot 2
 3. MySQL 8.4
 # spring boot的构造器
 ## spring官方的：https://start.spring.io/
---
+++ springboot_04_junit/src/main/java/com/example/dao/impl/BookDaoImpl.java
@@ -0,0 +1,12 @@
+package com.example.dao.impl;
+
+import com.example.dao.BookDao;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookDaoImpl implements BookDao {
+    @Override
+    public void save() {
+        System.out.println("book dao is running ...");
+    }
+}
